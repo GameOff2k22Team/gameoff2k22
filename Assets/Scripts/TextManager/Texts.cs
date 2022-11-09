f22513d332d64bd10e67876753c2fb0002a31fcb
Maxime Gammaitoni <maxime.gammaitoni@technologypartner.io>
2022-11-09 20:58:45 +0100

    feature/textManager_for_john

---
+++ Assets/Scripts/TextManager/Texts.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.StepTexts
+{
+    [System.Serializable]
+    public class StepTexts
+    {
+        public Step[] steps;
+    }
+
+    [System.Serializable]
+    public class Step
+    {
+        public string name;
+        [SerializeField]
+        public string[] texts;
+    }
+}
