43f4469107019b2975323688d8ef8375369c492d
Jipey <chevalier.lancioni.jp@gmail.com>
2022-11-13 20:32:55 +0100

    [Feat] Make GameEventListeners serializable in scripts

--- Assets/Scripts/GameEvents/GameEvent.cs
+++ Assets/Scripts/GameEvents/GameEvent.cs
@@ -11,22 +11,36 @@ namespace Architecture
         /// <summary>
         /// The list of listeners that this event will notify if it is raised.
         /// </summary>
-        private readonly List<GameEventListener> eventListeners =
-            new List<GameEventListener>();
+        private readonly List<IGameEventListener> eventListeners =
+            new List<IGameEventListener>();
 
         public void Raise()
         {
+            Debug.Log("Raised");
             for (int i = eventListeners.Count - 1; i >= 0; i--)
+            {
+                Debug.Log("InLoopRaise");
                 eventListeners[i].OnEventRaised();
+
+            }
+
         }
 
-        public void RegisterListener(GameEventListener listener)
+        public void Raise<T>(T input)
+        {
+            Debug.Log("GenericRaised");
+
+            for (int i = eventListeners.Count - 1; i >= 0; i--)
+                eventListeners[i].OnEventRaised(input);
+        }
+
+        public void RegisterListener(IGameEventListener listener)
         {
             if (!eventListeners.Contains(listener))
                 eventListeners.Add(listener);
         }
 
-        public void UnregisterListener(GameEventListener listener)
+        public void UnregisterListener(IGameEventListener listener)
         {
             if (eventListeners.Contains(listener))
                 eventListeners.Remove(listener);
