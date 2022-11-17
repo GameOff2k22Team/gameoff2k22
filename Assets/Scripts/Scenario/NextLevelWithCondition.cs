7c87afc8669248b6f13a4bb7f0ab52c95b6ea8d4
ManonSutter <suttermanon57@gmail.com>
2022-11-17 23:51:23 +0100

    Add Managers, and doos in bedroom, InHouse and Forest
    Need to add the trigger for the interaction

--- Assets/Scripts/Scenario/NextLevelWithCondition.cs
+++ Assets/Scripts/Scenario/NextLevelWithCondition.cs
@@ -1,14 +1,21 @@
 using Architecture;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class NextLevelWithCondition : NextLevel
 {
     public BoolVariable condition;
 
+    [Space]
+    public UnityEvent OnWrongCondition;
     public override void GoToNextScene()
     {
         if (condition.Value)
         {
             base.GoToNextScene();
+        } else
+        {
+            OnWrongCondition?.Invoke();
         }
     }
 }
