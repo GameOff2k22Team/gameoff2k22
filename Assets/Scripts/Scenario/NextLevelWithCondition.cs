173ca4f41e3283da78097bbb26aaaa782aa5c2c9
ManonSutter <suttermanon57@gmail.com>
2022-11-28 22:31:10 +0100

    Fix bubble scale, mum dialogue, and door not locked

--- Assets/Scripts/Scenario/NextLevelWithCondition.cs
+++ Assets/Scripts/Scenario/NextLevelWithCondition.cs
@@ -7,12 +7,14 @@ public class NextLevelWithCondition : NextLevel
     public BoolVariable condition;
 
     [Space]
+    public UnityEvent OnRightCondition;
     public UnityEvent OnWrongCondition;
     public override void GoToNextScene()
     {
         if (condition.Value)
         {
             base.GoToNextScene();
+            OnRightCondition?.Invoke();
         } else
         {
             OnWrongCondition?.Invoke();
