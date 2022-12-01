4450a2e64556bd5dbe9d45a6e976038ab282c442
Jipey <chevalier.lancioni.jp@gmail.com>
2022-12-01 21:50:17 +0100

    Add daronne animations

--- Assets/Scripts/Boss/BossManager.cs
+++ Assets/Scripts/Boss/BossManager.cs
@@ -18,6 +18,12 @@ public class BossManager : MonoBehaviour
     private GameEvent OnPlayerMustNotDie;
 
     [SerializeField]
+    private Animator MotherP12;
+
+    [SerializeField]
+    private Animator MotherP3;
+
+    [SerializeField]
     private BossEnemy enemy;
 
     [SerializeField]
@@ -144,6 +150,7 @@ public class BossManager : MonoBehaviour
 
     private void CallWhenPlayerDies()
     {
+        MotherP12.SetBool("isAttacking", false);
         ClearEnemies();
         StopAllCoroutines();
     }
@@ -292,11 +299,13 @@ public class BossManager : MonoBehaviour
     private IEnumerator Phase2Coroutine()
     {
         yield return StartCoroutine(WaitBeforeStart());
+        MotherP12.SetBool("isAttacking", true);
         yield return StartCoroutine(P2PatternCoroutine(p2S1Pattern, _numberOfPatternP2S1, false, _P2S1SpawningSpeed));
         yield return new WaitForSeconds(10f);
         yield return StartCoroutine(P2PatternCoroutine(p2S2Pattern, _numberOfPatternP2S2, false, _P2S2SpawningSpeed));
         yield return new WaitForSeconds(10f);
         yield return StartCoroutine(P2PatternCoroutine(p2S3Pattern, _numberOfPatternP2S3, false, _P2S3SpawningSpeed));
+        MotherP12.SetBool("isAttacking", false);
         yield return new WaitForSeconds(10f);
         ClearEnemies();
         yield return new WaitForSeconds(2f);
@@ -355,6 +364,7 @@ public class BossManager : MonoBehaviour
         var i = 0;
         while (i < BossPatterns.Count)
         {
+            MotherP3.SetTrigger("Attack");
             PatternBossP3 currentBossPattern = BossPatterns[i];
             SFXProjectileP3.Post(gameObject);
             LineAreaManager.Instance.ToggleLines(currentBossPattern.nbOfVertLines, 
